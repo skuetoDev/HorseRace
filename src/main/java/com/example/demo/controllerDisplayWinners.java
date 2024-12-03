ba94778f509382b7ff52bd39ec7a259a8960b632
mariognDev <mariogn1987@gmail.com>
2024-12-03 13:42:54 +0100

    update git ignore

---
+++ src/main/java/com/example/demo/controllerDisplayWinners.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class controllerDisplayWinners extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
